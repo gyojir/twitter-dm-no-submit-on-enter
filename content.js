3eb96ba5b471abe75c55fdc24b61c819d91ad390
gyojir <gyojir@users.noreply.github.com>
2022-04-17 18:21:20 +0900

    挙動変更対応

--- content.js
+++ content.js
@@ -1,4 +1,13 @@
-document.onkeydown = (e)=> {
+const checkIntervalID = setInterval(check, 1000);
+function check() {
+    const elem = document.querySelector('[data-testid~="DMDrawer"]');
+    if(elem != null) {
+        elem.onkeydown = onkeydown;
+        clearInterval(checkIntervalID);
+    }
+}
+
+function onkeydown(e) {
     if(e.key === 'Enter' && !e.shiftKey && e.target.classList.contains("public-DraftEditor-content")) {
         e.target.dispatchEvent(new KeyboardEvent('keydown', {'key':'Enter', 'code': 'Enter', which: 13, keyCode: 13, bubbles: true, shiftKey: true}));
         e.preventDefault();
